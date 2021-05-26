499a5806b498816fe26158cfa939721c1f1415e5
Antônio Fernando <58742084+fernanndosenna@users.noreply.github.com>
2021-05-25 23:58:23 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 //ARQUIVO DE CONEXAO COM O BANCO DE DADOS//
 const sequelize = require('sequelize');
 
-const connection = new sequelize('guiapress', 'fernando','Hertz94773195',{
+const connection = new sequelize('guiapress', 'fernando','password',{
     host: 'localhost',
     dialect: 'mysql',
     timezone: "-3:00" //horario do brasil
